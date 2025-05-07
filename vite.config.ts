f985ebc9adae62ba2c35ea31a439e5178a46be4c
sebrode <sebastian_rode@yahoo.dk>
2025-05-07 11:29:58 +0200

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // listen on all network interfaces
+    port: 5173,       // optional if you want to be explicit
+  },
 })
